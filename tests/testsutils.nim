d58baefd75100a49e8eda6801f80e4740876eadd
rockcavera <rockcavera@gmail.com>
2023-05-30 21:51:07 -0300

    little improvements in tests

--- tests/testsutils.nim
+++ tests/testsutils.nim
@@ -1,3 +1,18 @@
+template initTests*(seeds: openarray[int]) =
+  import std/[os, strutils, unittest]
+
+  # Prevents compile-time optimizations (?)
+  # Allows you to pass other seeds
+  var
+    tseed {.inject.} = seeds
+    length = paramCount()
+
+  if length > len(tseed):
+    length = len(tseed)
+
+  for i in 1 .. length:
+    tseed[i - 1] = paramStr(i).parseInt()
+
 template testRng*(init: untyped) =
   var
     rng = init
--- tests/tjsf.nim
+++ tests/tjsf.nim
@@ -1,22 +1,15 @@
-import std/[os, strutils, unittest]
-
 import ./randnimgulins/jsf
 
 import ./testsutils
 
-# Prevents compile-time optimizations (?)
-# Allows you to pass other seeds
-var tseed = 0
-
-if paramCount() > 0:
-  tseed = paramStr(1).parseInt()
+initTests([0])
 
 suite "Jenkins's Small Fast RNG (JSF)":
   test "JSF32":
     const expected = [446393351'u32, 2589264021'u32, 4046186614'u32, 151173657'u32, 552706628'u32]
 
-    testRng(initJsf32(tseed.uint32))
+    testRng(initJsf32(tseed[0].uint32))
   test "JSF64":
     const expected = [5420579327082221045'u64, 12601856710328663849'u64, 3486099297865454798'u64, 9209813893562929851'u64, 13082810583377980795'u64]
 
-    testRng(initJsf64(tseed.uint64))
+    testRng(initJsf64(tseed[0].uint64))
